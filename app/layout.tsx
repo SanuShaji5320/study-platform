1eff1bafbca64b3c3f89cb7368d3e9cdaf019f7f
Sanu Shaji <sanuzane15@gmail.com>
2026-04-13 19:16:56 +0530

    fixed ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,29 @@
-<nav className="bg-white border-b px-6 py-4 flex justify-between items-center">
-  <h1 className="text-xl font-bold">StudySmart</h1>
+import "./globals.css";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-50">
 
-  <div className="flex gap-6 text-sm font-medium">
-    <a href="/" className="hover:text-blue-600">Home</a>
-    <a href="/syllabus" className="hover:text-blue-600">Syllabus</a>
-    <a href="/subjects" className="hover:text-blue-600">notes</a>
-    <a href="/mock-test" className="hover:text-blue-600">Practice</a>
-  </div>
-</nav>
+        {/* Navbar */}
+        <nav className="bg-white border-b px-6 py-4 flex justify-between items-center">
+          <h1 className="text-xl font-bold">StudySmart</h1>
+
+          <div className="flex gap-6 text-sm font-medium">
+            <a href="/" className="hover:text-blue-600">Home</a>
+            <a href="/syllabus" className="hover:text-blue-600">Syllabus</a>
+            <a href="/subjects" className="hover:text-blue-600">Notes</a>
+            <a href="/mock-test" className="hover:text-blue-600">Practice</a>
+          </div>
+        </nav>
+
+        {/* Page Content */}
+        {children}
+
+      </body>
+    </html>
+  );
+}
